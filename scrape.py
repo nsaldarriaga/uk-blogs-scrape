1cd713a392500674956484cfc2a22add5141547c
Neptali <neptali.j.saldarriaga@gmail.com>
2025-03-30 09:43:16 -0300

    agrega cambios archivo scrape.py

--- scrape.py
+++ scrape.py
@@ -2,6 +2,8 @@ import json
 from collections import defaultdict
 from datetime import datetime, timedelta
 from pathlib import Path
+import time
+import random 
 
 import requests  # type: ignore
 from bs4 import BeautifulSoup # type: ignore
@@ -56,13 +58,16 @@ def fetch_entries_for_page(page_number, cutoff_date):
 
 all_current_entries = []
 
-for page in range(1, 10_000):
+for page in range(1, 101):
     print(f'Web scrapping page {page}')
     entries = fetch_entries_for_page(page, cutoff_date)
     if not entries:
        break
     all_current_entries.extend(entries)
 
+delay = random.uniform(1, 3)
+print(f'Esperando {delay:.2f} segundos antes de la siguiente página...')
+time.sleep(delay)
 
 def parse_article_metadata(article_soup):
     article_tag = article_soup.find('article')
